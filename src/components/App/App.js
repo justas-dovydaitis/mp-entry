f291f4a6c86c247f965c2b1efdab3cf686d4fe9c
Justas Dovydaitis <justuxlt1@gmail.com>
2020-01-05 21:26:56 +0200

    project init

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='row'>
+        <div className='col'>
+          {/* search grid */}
+        </div>
+        <div className='col'>
+          {/* save, queries */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
