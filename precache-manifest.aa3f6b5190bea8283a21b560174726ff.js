5b6579b51e50d3db1b38b164198e1c64b2cb78ed
Justas Dovydaitis <justuxlt1@gmail.com>
2020-01-06 05:50:26 +0200

    Updates

---
+++ precache-manifest.aa3f6b5190bea8283a21b560174726ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074301e2014c16da13466e5848d2173f",
+    "url": "/mp-entry/index.html"
+  },
+  {
+    "revision": "c13b8650bce23fa1d180",
+    "url": "/mp-entry/static/css/main.0b473c29.chunk.css"
+  },
+  {
+    "revision": "b23c65803bf7f5b608f5",
+    "url": "/mp-entry/static/js/2.d4f6b799.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/mp-entry/static/js/2.d4f6b799.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c13b8650bce23fa1d180",
+    "url": "/mp-entry/static/js/main.b694ae6d.chunk.js"
+  },
+  {
+    "revision": "69b42bd3b172f591218a",
+    "url": "/mp-entry/static/js/runtime-main.9b0a4bdd.js"
+  }
+]);
